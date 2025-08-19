b71803bf3024b2c395d3105f3523995fefb30d7f
Filipe Silva <filipej.f.1020@gmail.com>
2025-08-19 02:23:33 -0300

    pushing all project

---
+++ Business/DTOs/DashboardStats.cs
@@ -0,0 +1,9 @@
+namespace BarbersClub.Business.DTOs;
+
+public class DashboardStats
+{
+    public int ServicesToday { get; set; }
+    public decimal RevenueToday { get; set; }
+    public int NewClientsMonth { get; set; }
+    public decimal AverageRating { get; set; }
+}   
---
+++ Business/DTOs/ServiceViewResponse.cs
@@ -0,0 +1,25 @@
+namespace BarbersClub.Business.DTOs;
+
+public record ServiceViewResponse
+{
+    public int? ServiceId { get; set; }
+    public DateTime Date { get; set; }
+    public TimeSpan Time { get; set; }
+    public string ServiceType { get; set; }
+    
+    public string Status { get; set; }
+    public int BarberShopId { get; set; }
+    public string BarberShopName { get; set; } 
+    public int ClientId { get; set; }
+    public string ClientName { get; set; }
+    
+    public string Barber { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    
+    public string? ServiceImageUrl { get; set; }
+    
+    public bool HasRating { get; set; }
+        
+    public List<OfferedServiceResponse> OfferedServices { get; set; }
+}
