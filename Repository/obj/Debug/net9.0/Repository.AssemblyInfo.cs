e298120d2de53ae748349c0a1356d14e5dd48f65
Filipe Silva <filipej.f.1020@gmail.com>
2025-08-24 17:01:37 -0300

    updating namespaces, removing and updating views

--- Repository/obj/Debug/net9.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net9.0/Repository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a9bb85a3680bcf8f2bcd1143da2b6fa771493c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2b8cb722349b54dcc3358f55e780f432ef37234")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
